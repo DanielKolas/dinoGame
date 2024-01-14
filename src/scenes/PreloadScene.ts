dc95f84c150f9d80861ba721123e64ab92b1c9ad
Daniel Kolas <daniel.piotr.kolas@gmail.com>
2024-01-14 20:08:34 +0100

    added dinoDown animation

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -13,15 +13,18 @@ class PreloadScene extends Phaser.Scene {
         this.load.image("restart", "assets/restart.png");
         this.load.image("game-over", "assets/game-over.png");
 
-
         for( let i = 1; i < PRELOAD_CONFIG.cactusesCount + 1; i++){
             this.load.image(`obstacle-${i}`, `assets/cactuses_${i}.png`)
             console.log("obstacle "+ i + " loaded")
         }
 
-
         this.load.spritesheet("dino-run", "assets/dino-run.png",{
             frameWidth: 88,
+            frameHeight: 94
+        });
+
+        this.load.spritesheet("dino-down", "assets/dino-down-2.png",{
+            frameWidth: 118,
             frameHeight: 94
         });
     }
